0f3d84f3dd1f06f9a53ae8c3876f6a9e7dfd87e7
abcdefg <78954867+61bcdefg@users.noreply.github.com>
2023-08-21 11:51:05 +0800

    initialize

---
+++ machostrip/main.cpp
@@ -0,0 +1,87 @@
+//
+//  main.cpp
+//
+//  Created by 123456qwerty on 2023/8/11.
+//
+
+#include "LIEF/LIEF.hpp"
+#include <fstream>
+#include <iostream>
+#include <mach-o/loader.h>
+#include <random>
+#include <string>
+
+using namespace LIEF::MachO;
+
+int main(int argc, const char *argv[]) {
+  if (argc < 3) {
+    std::cout << "Usage: machostrip [mach-o file] [output file]" << std::endl;
+    return 1;
+  }
+
+  std::unique_ptr<FatBinary> Binaries = Parser::parse(argv[1]);
+  for (Binary &Bin : *Binaries) {
+    // remove function starts
+    Bin.function_starts()->functions({});
+    // remove local symbols
+    std::vector<Symbol *> symtoremove;
+    for (Symbol &Sym : Bin.symbols()) {
+      if (Sym.category() == Symbol::CATEGORY::LOCAL)
+        symtoremove.emplace_back(&Sym);
+    }
+    for (Symbol *Sym : symtoremove)
+      Bin.remove(*Sym);
+    for (SegmentCommand &Seg : Bin.segments()) {
+      if (Seg.name() == "__TEXT" || Seg.name() == "__DATA" ||
+          Seg.name() == "__DATA__CONST")
+        for (Section &Sec : Seg.sections()) {
+          if (Sec.name().find("__objc") == std::string::npos &&
+              Sec.name().find("__swift") == std::string::npos &&
+              Sec.name().find("__unwind") == std::string::npos &&
+              Sec.name().find("__eh") == std::string::npos &&
+              Sec.name().find("__gcc") == std::string::npos &&
+              Sec.name().find("__auth") == std::string::npos &&
+              Sec.name().find("__got") == std::string::npos) {
+            // malformed section name can prevent Ghidra from loading the macho
+            Sec.name("\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11\x11"
+                     "\x11\x11");
+          }
+        }
+    }
+    // Hopper Demo Version checks if the binary contains this string, and if it
+    // does, disassembly is not allowed
+    Bin.add_exported_function(
+        0, "(c) 2014 - Cryptic Apps SARL - Disassembling not allowed.");
+  }
+  const std::string output_name = argv[2];
+  Binaries->write(output_name);
+
+  // obfuscate symbol stub name
+  std::set<uint32_t> stroffs;
+  std::map<uint32_t, uint32_t> strtabsize;
+
+  std::unique_ptr<FatBinary> Binaries2 = Parser::parse(output_name);
+  for (Binary &Bin : *Binaries2) {
+    uint32_t stroff =
+        (uint32_t)Bin.fat_offset() + Bin.symbol_command()->strings_offset();
+    stroffs.insert(stroff);
+    strtabsize[stroff] = Bin.symbol_command()->strings_size();
+  }
+
+  std::mt19937 eng(rand());
+  std::uniform_int_distribution<uint8_t> dis(1, 0xff);
+
+  std::fstream file;
+  file.open(output_name, std::ios::in | std::ios::out);
+  for (uint32_t off : stroffs) {
+    uint32_t temp = off + strtabsize[off];
+    while (off != temp) {
+      file.seekp(off, std::ios::beg);
+      file << dis(eng);
+      off += 1;
+    }
+  }
+  file.close();
+
+  return 0;
+}
